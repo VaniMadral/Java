68e586a43d6e178febe6d3e8d312c4241275d70b
VaniMadral <vanimadral@gmail.com>
2025-03-12 13:50:48 +0530

    Add files via upload

---
+++ Decr_4_11to_33.java
@@ -0,0 +1,10 @@
+package loops_pgms;
+
+public class Decr_4_11to_33 {
+	//to print 11 to -33 decrement number by 4
+	public static void main(String[] args) {
+		for(int i=11;i>=-33;i-=4) {
+			System.out.println(i);
+		}
+	}
+}
