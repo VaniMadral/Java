32f2d1c6f3ecb23589c9d182294ce4f8dce69e03
VaniMadral <vanimadral@gmail.com>
2025-03-12 13:51:10 +0530

    Add files via upload

---
+++ DoWhile_1to10.java
@@ -0,0 +1,14 @@
+package loops_pgms;
+
+public class DoWhile_1to10 {
+
+	public static void main(String[] args) {
+		// to print 1 to 10 using do while loop
+		int k=1;
+		do {
+			System.out.println(k);
+			k++;
+		}
+		while(k<=10);
+	}
+}
